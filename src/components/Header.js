83ba6cb64fcc3e8454cb9c3470bba52beea81b61
robinucar <mehmet.ucar@imediapro.co.uk>
2021-01-27 19:49:25 +0000

    Button component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,9 @@
+import Button from './Button';
 const Header = ({ title }) => {
   return (
     <header className='header'>
       <h1>{title}</h1>
+      <Button color='green' text='Add' />
     </header>
   );
 };
